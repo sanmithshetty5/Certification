25104cb316bb9da0e9db674d9c2fdd12e6710b2f
Kirthi Shetty <147197142+rustyonkeys@users.noreply.github.com>
2026-01-16 13:15:33 +0530

    Update 2_Realtime_Analysis.py

--- pages/2_Realtime_Analysis.py
+++ pages/2_Realtime_Analysis.py
@@ -393,10 +393,10 @@ with header_left:
     </div>
     """, unsafe_allow_html=True)
 
-with header_right:
-    st.markdown("<br>", unsafe_allow_html=True)
-    st.button("⬇ Export Data")
-    st.button("➕ Assign Voucher")
+# with header_right:
+#     st.markdown("<br>", unsafe_allow_html=True)
+#     st.button("⬇ Export Data")
+#     st.button("➕ Assign Voucher")
 
 st.markdown("<br>", unsafe_allow_html=True)
 
