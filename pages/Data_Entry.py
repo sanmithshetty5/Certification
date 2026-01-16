74371318381cb82e50072b9eaf7853c79aa092da
sanmithshetty5 <sanmithshetty5@gmail.com>
2026-01-16 15:00:05 +0530

    Update Data_Entry.py

--- pages/Data_Entry.py
+++ pages/Data_Entry.py
@@ -44,7 +44,7 @@ st.markdown("""
     }
     
     .app-logo img {
-        height: 10px;       /* adjust if needed */
+        height: 1000px;       /* adjust if needed */
         width: auto;
     }
 
