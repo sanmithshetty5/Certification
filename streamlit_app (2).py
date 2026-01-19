bb917800f4711ac59f7ced59577ba116c84d40e3
sanmithshetty5 <sanmithshetty5@gmail.com>
2026-01-19 11:02:12 +0530

    Update streamlit_app (2).py

--- streamlit_app (2).py
+++ streamlit_app (2).py
@@ -1,26 +1,26 @@
-import streamlit as st
+# import streamlit as st
 
-st.set_page_config(
-    page_title="Certification Management System",
-    layout="wide"
-)
+# st.set_page_config(
+#     page_title="Certification Management System",
+#     layout="wide"
+# )
 
-st.sidebar.title("🎓 Certification Portal")
+# st.sidebar.title("🎓 Certification Portal")
 
-page = st.sidebar.radio(
-    "Navigate",
-    [
-        "🏠 Welcome",
-        "✍️ Certification Tracker",
-        "📊 Certification Analytics"
-    ]
-)
+# page = st.sidebar.radio(
+#     "Navigate",
+#     [
+#         "🏠 Welcome",
+#         "✍️ Certification Tracker",
+#         "📊 Certification Analytics"
+#     ]
+# )
 
-if page == "🏠 Welcome":
-    st.switch_page("pages/Welcome_Page.py")
+# if page == "🏠 Welcome":
+#     st.switch_page("pages/Welcome_Page.py")
 
-elif page == "✍️ Certification Tracker":
-    st.switch_page("pages/Data_Entry.py")
+# elif page == "✍️ Certification Tracker":
+#     st.switch_page("pages/Data_Entry.py")
 
-elif page == "📊 Certification Analytics":
-    st.switch_page("pages/2_Realtime_Analysis.py")
+# elif page == "📊 Certification Analytics":
+#     st.switch_page("pages/2_Realtime_Analysis.py")
