099005bbcc776b319d6a28d488e5642d4859b947
sanmithshetty5 <sanmithshetty5@gmail.com>
2026-01-20 12:26:02 +0530

    Update 2_Realtime_Analysis.py

--- pages/2_Realtime_Analysis.py
+++ pages/2_Realtime_Analysis.py
@@ -112,42 +112,7 @@ st.markdown(f"""
         margin-top: 5px;
     }}
 
-    # /* 6. SIDEBAR REFINEMENT */
-    # section[data-testid="stSidebar"] {{
-    #     background-color: #0F172A !important; /* Dark navy */
-    #     border-right: 1px solid #1E293B;
-    # }}
-    # section[data-testid="stSidebar"] h1,
-    # section[data-testid="stSidebar"] h2,
-    # section[data-testid="stSidebar"] h3,
-    # section[data-testid="stSidebar"] h4 {{
-    #     color: #FFFFFF !important;
-    #     font-weight: 600;
-    # }}
-    # /* Sidebar labels */
-    # section[data-testid="stSidebar"] label,
-    # section[data-testid="stSidebar"] label p {{
-    #     color: #FFFFFF !important;
-    #     font-weight: 500;
-    # }}
-
-    # /* Sidebar input text */
-    # section[data-testid="stSidebar"] input,
-    # section[data-testid="stSidebar"] textarea {{
-    #     color: #0F172A !important;
-    #     background-color: #FFFFFF !important;
-    # }}
     
-    # /* Sidebar selectbox */
-    # section[data-testid="stSidebar"] div[data-baseweb="select"] {{
-    #     background-color: #FFFFFF !important;
-    #     color: #0F172A !important;
-    # }}
-    
-    # /* Sidebar divider lines */
-    # section[data-testid="stSidebar"] hr {{
-    #     border-color: #334155;
-    # }}
 
     /* 7. MATPLOTLIB TRANSPARENCY FIX */
     .chart-title {{
@@ -170,6 +135,24 @@ st.markdown(f"""
         background-color: #4ED95E;
         transform: translateY(-3px);
         color: #ffffff !important;
+    }}
+
+        /* EXPANDER HEADER */
+    div[data-testid="stExpander"] summary {{
+        background-color: transparent;
+        color: #1e293b;
+        border-radius: 6px;
+    }}
+    
+    /* HOVER */
+    div[data-testid="stExpander"] summary:hover {{
+        background-color: #e2e8f0;  /* Light slate */
+    }}
+    
+    /* EXPANDER CONTENT */
+    div[data-testid="stExpander"] > div {{
+        background-color: #ffffff;
+        border-radius: 6px;
     }}
 
 </style>
