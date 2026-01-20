c1bb0bd2b4abb35485f10fa7f20e76ad7db781f9
sanmithshetty5 <sanmithshetty5@gmail.com>
2026-01-20 12:38:28 +0530

    Update 2_Realtime_Analysis.py

--- pages/2_Realtime_Analysis.py
+++ pages/2_Realtime_Analysis.py
@@ -195,7 +195,7 @@ df["Completed Flag"] = df["Actual Date of completion"].notna()
 # SIDEBAR
 # -----------------------------------------
 with st.sidebar:
-    st.image("https://github.com/sanmithshetty5/Certification/blob/main/pages/logo.png", width=50) # Placeholder Logo
+    st.image("https://raw.githubusercontent.com/sanmithshetty5/Certification/main/pages/logo.png", width=50) # Placeholder Logo
     st.markdown("## Analytics Console")
     st.markdown("---")
     
