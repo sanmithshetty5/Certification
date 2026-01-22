23c2b07e148f538df178a365312e4c31d0556696
Kirthi Shetty <147197142+rustyonkeys@users.noreply.github.com>
2026-01-22 14:28:24 +0530

    Update About_Page.py

--- pages/About_Page.py
+++ pages/About_Page.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(
     page_title="About | Employee Certification Portal",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    # initial_sidebar_state="collapsed"
 )
 
 # -------------------------------
